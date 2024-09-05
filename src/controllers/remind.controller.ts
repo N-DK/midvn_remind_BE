d3d9b29985469d11904741c149b2d8317e9c40eb
N_DK <nguyenkhoadang135759@gmail.com>
2024-09-05 15:51:08 +0700

    [POST] add a remind

--- src/controllers/remind.controller.ts
+++ src/controllers/remind.controller.ts
@@ -1,10 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../helper/catchAsync.helper';
 import remindService from '../services/remind.service';
-import { GET } from '../core/success.response';
+import { CREATED, GET } from '../core/success.response';
 
 class RemindController {
-
     getAll = catchAsync(
         async (req: Request, res: Response, next: NextFunction) => {
             const data = await remindService.getAll(req.body.user.user_id);
@@ -12,6 +11,13 @@ class RemindController {
         },
     );
 
+    addRemind = catchAsync(
+        async (req: Request, res: Response, next: NextFunction) => {
+            const data = req.body;
+            const remind = await remindService.addRemind(data);
+            CREATED(res, remind);
+        },
+    );
 }
 
 export default new RemindController();
