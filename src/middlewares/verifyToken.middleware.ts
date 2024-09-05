399daa806fa0de08851a53319bfb001adb0831f5
N_DK <nguyenkhoadang135759@gmail.com>
2024-09-05 11:24:25 +0700

    update

--- src/middlewares/verifyToken.middleware.ts
+++ src/middlewares/verifyToken.middleware.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { Api401Error } from '../core/error.response';
 import configureEnvironment from '../config/dotenv.config';
+import { buffer } from 'stream/consumers';
 
 const { JWT_SECRET_KEY } = configureEnvironment();
 
@@ -14,11 +15,21 @@ export const verifyToken = (
     if (!token) {
         return next(new Api401Error('No token provided'));
     }
-    jwt.verify(token, JWT_SECRET_KEY as string, (err: any, user: any) => {
-        if (err) {
-            return next(new Api401Error('Unauthorized'));
-        }
+    // buffer from base64
+    const user = JSON.parse(
+        Buffer.from(token.split('.')[1], 'base64').toString(),
+    );
+    if (user) {
         req.body.user = user;
         next();
-    });
+    } else {
+        return next(new Api401Error('Unauthorized'));
+    }
+    // jwt.verify(token, JWT_SECRET_KEY as string, (err: any, user: any) => {
+    //     if (err) {
+    //         return next(new Api401Error('Unauthorized'));
+    //     }
+    //     req.body.user = user;
+    //     next();
+    // });
 };
