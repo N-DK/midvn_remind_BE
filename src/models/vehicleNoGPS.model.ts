fa70b9ef2d0cf521836b31e43cb1c237ec322abd
NguyenThinh <koulse.kudaza@gmail.com>
2024-09-06 15:09:43 +0700

    update api search

--- src/models/vehicleNoGPS.model.ts
+++ src/models/vehicleNoGPS.model.ts
@@ -90,11 +90,55 @@ class VehicleNoGPS extends DatabaseModel {
     return result;
   }
   async search(con: PoolConnection, data: any, user_id: number) {
-    const result = await this.select(
-        con, 
-        tables.tableVehicleNoGPS,
-        '*',
-        `user_id = ${user_id} AND (license_plate LIKE '%${data.keyword}%' OR license LIKE '%${data.keyword}%') AND is_deleted = 0`
+    const result = await this.selectWithJoins(
+      con,
+      tables.tableVehicleNoGPS,
+      `${tables.tableVehicleNoGPS}.license_plate AS license_plate,
+                 ${tables.tableVehicleNoGPS}.user_id AS user_id,
+                 ${tables.tableVehicleNoGPS}.license AS license,
+                 ${tables.tableVehicleNoGPS}.create_time AS vehicle_create_time,
+                 ${tables.tableVehicleNoGPS}.update_time AS vehicle_update_time,
+                 
+                 ${tables.tableRemind}.id AS remind_id,
+                 ${tables.tableRemind}.img_url AS remind_img_url,
+                 ${tables.tableRemind}.note_repair AS note_repair,
+                 ${tables.tableRemind}.history_repair AS history_repair,
+                 ${tables.tableRemind}.current_kilometers AS current_kilometers,
+                 ${tables.tableRemind}.cumulative_kilometers AS cumulative_kilometers,
+                 ${tables.tableRemind}.expiration_time AS expiration_time,
+                 ${tables.tableRemind}.time_before AS time_before,
+                 ${tables.tableRemind}.is_notified AS is_notified,
+                 ${tables.tableRemind}.is_received AS is_received,
+                 ${tables.tableRemind}.create_time AS remind_create_time,
+                 ${tables.tableRemind}.update_time AS remind_update_time,
+                 
+                 ${tables.tableRemindCategory}.id AS category_id,
+                 ${tables.tableRemindCategory}.name AS category_name,
+                 ${tables.tableRemindCategory}.desc AS category_desc,
+                 ${tables.tableRemindCategory}.icon AS category_icon,
+                 ${tables.tableRemindCategory}.create_time AS category_create_time,
+                 ${tables.tableRemindCategory}.update_time AS category_update_time,
+                 ${tables.tableRemindCategory}.is_deleted AS category_is_deleted`,
+
+      `${tables.tableVehicleNoGPS}.user_id = ? AND (${tables.tableVehicleNoGPS}.license_plate LIKE '%${data.keyword}%' OR ${tables.tableVehicleNoGPS}.license LIKE '%${data.keyword}%') AND ${tables.tableVehicleNoGPS}.is_deleted = 0`,
+      [user_id],
+      [
+        {
+          table: tables.tableRemindVehicle,
+          on: `${tables.tableVehicleNoGPS}.license_plate = ${tables.tableRemindVehicle}.vehicle_id`,
+          type: "INNER",
+        },
+        {
+          table: tables.tableRemind,
+          on: `${tables.tableRemindVehicle}.remind_id = ${tables.tableRemind}.id`,
+          type: "INNER",
+        },
+        {
+          table: tables.tableRemindCategory,
+          on: `${tables.tableRemind}.remind_category_id = ${tables.tableRemindCategory}.id`,
+          type: "INNER",
+        },
+      ]
     );
     return result;
   }
