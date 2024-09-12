cee47627089d8d3a46db1558d3b50b1cdc566805
N_DK <nguyenkhoadang135759@gmail.com>
2024-09-12 20:06:36 +0700

    update

--- src/utils/mqtt/features/remindFeature.feature.ts
+++ src/utils/mqtt/features/remindFeature.feature.ts
@@ -8,42 +8,50 @@ const { SV_NOTIFY } = configureEnvironment();
 
 const remindFeature = async (client: any, data: any, requestId: any) => {
     const isRedisReady = redisModel.redis.instanceConnect.isReady;
-    let reminds: any = [];
 
     try {
+        // Kiểm tra nếu không có dữ liệu hợp lệ
         if (!data || !Object.keys(data).length) return;
 
+        let reminds: any[] = [];
+
         if (isRedisReady) {
+            // Lấy remind từ Redis nếu Redis sẵn sàng
             reminds =
-                (await reminder.groupVehiclesWithObjects(data.imei)) ?? [];
-        }
-
-        if (reminds.length === 0) {
+                (await reminder.groupVehiclesWithObjects(data.imei)) || [];
+        } else {
+            // Lấy remind từ nguồn khác nếu Redis không sẵn sàng
             const result = await reminder.getReminds();
-            reminds = result[data.imei] ?? [];
+            reminds = result[data.imei] || [];
         }
 
+        // Nếu có remind, tiến hành xử lý
         if (reminds.length > 0) {
-            console.log(`${data.vehicle_name} - ${reminds.length}`);
-            for (const remind of reminds) {
-                const isOverKm =
-                    data.total_distance >=
-                    remind.current_kilometers +
-                        remind.cumulative_kilometers -
-                        remind.km_before;
-
-                if (isOverKm) {
-                    await remindNotify.sendNotifyRemind(SV_NOTIFY as string, {
-                        name_remind: 'Vượt quá số km bảo dưỡng',
-                        vehicle_name: data.vehicle_name,
-                        user_id: remind.user_id, //remind.user_id,
-                    });
-                }
-            }
+            await processRemind(data, reminds);
         }
     } catch (error) {
-        console.log('Error', error);
+        console.error('Error', error);
         mylogger.error('message', ['nameFeature', requestId, error]);
+    }
+};
+
+const processRemind = async (data: any, reminds: any[]) => {
+    console.log(`${data.vehicle_name} - ${reminds.length}`);
+
+    for (const remind of reminds) {
+        const isOverKm =
+            data.total_distance >=
+            remind.current_kilometers +
+                remind.cumulative_kilometers -
+                remind.km_before;
+
+        if (isOverKm) {
+            await remindNotify.sendNotifyRemind(SV_NOTIFY as string, {
+                name_remind: 'Vượt quá số km bảo dưỡng',
+                vehicle_name: data.vehicle_name,
+                user_id: remind.user_id,
+            });
+        }
     }
 };
 
